8a45942b882835e71c1787ac4368e35fc5113e6c
Miller Villamizar <mvillamizar@algeiba.com>
2024-07-09 18:22:17 -0300

    deplo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-// index.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const commentRoutes = require('./src/routes/commentRoutes');
 const knex = require('./src/db/knex');
+const cors = require('cors'); // Asegúrate de requerir el paquete cors
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: 'http://blog-cats-production.up.railway.app:5000' // Reemplaza 'http://tu-dominio.com' con el dominio de tu frontend
+})); // Añade el middleware cors aquí
 
 app.use(bodyParser.json());
 app.use('/api/comments', commentRoutes);
