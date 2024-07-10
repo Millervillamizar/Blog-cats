0197c10aaa63ea8ca9a2e9e4233ee7d4d51f4314
Miller Villamizar Cisneros <139370459+Millervillamizar@users.noreply.github.com>
2024-07-09 22:40:45 -0300

    crud

--- server/src/models/Comment.js
+++ server/src/models/Comment.js
@@ -1,16 +1,33 @@
-// src/models/Comment.js
 const knex = require('../db/knex');
 
 class Comment {
+  // Encontrar comentarios por nombre del artículo
   static async findByArticleName(articleName) {
     return knex('comments').where({ article_name: articleName }).select('*');
   }
 
+  // Crear un nuevo comentario
   static async create(comment) {
     return knex('comments').insert(comment).returning('*');
+  }
+
+  // Encontrar comentario por ID
+  static async findById(commentId) {
+    return knex('comments').where({ id: commentId }).first();
+  }
+
+  // Actualizar un comentario
+  static async update(commentId, newData) {
+    return knex('comments').where({ id: commentId }).update(newData).returning('*');
+  }
+
+  // Eliminar un comentario
+  static async delete(commentId) {
+    return knex('comments').where({ id: commentId }).del();
   }
 }
 
 module.exports = Comment;
+
 
 
