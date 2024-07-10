3997e69d4be5cb5c1fb441612699a3ca5d7eb950
Miller Villamizar Cisneros <139370459+Millervillamizar@users.noreply.github.com>
2024-07-09 22:33:54 -0300

    crud

--- server/src/routes/commentRoutes.js
+++ server/src/routes/commentRoutes.js
@@ -5,5 +5,7 @@ const commentController = require('../controllers/commentController');
 
 router.get('/:articleName', commentController.getCommentsByArticleName);
 router.post('/:articleName/add-comments', commentController.createComment);
+router.put('/:commentId/edit', commentController.updateComment); // Ruta para editar comentario
+router.delete('/:commentId/delete', commentController.deleteComment); // Ruta para eliminar comentario
 
 module.exports = router;
